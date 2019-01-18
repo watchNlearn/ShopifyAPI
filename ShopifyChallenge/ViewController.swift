4bf296e10875b1b1f80b54b3596d7a3a3f52c407
Caleb Lee <28caleblee@gmail.com>
2019-01-18 12:00:04 -0800

    Init Commit

--- ShopifyChallenge/ViewController.swift
+++ ShopifyChallenge/ViewController.swift
@@ -8,13 +8,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+struct ServerResponse: Decodable {
+    var custom_collections: [Custom_Collections]
 }
 
+struct Custom_Collections: Decodable {
+    var title: String
+}
+
+
+class ViewController: UITableViewController {
+    
+ 
+    
+    var tableArray = [String]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let jsonUrlString = "https://shopicruit.myshopify.com/admin/custom_collections.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6"
+        guard let url = URL(string: jsonUrlString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                let shopifydata = try JSONDecoder().decode(ServerResponse.self, from: data)
+                for collection in shopifydata.custom_collections {
+                    print(collection.title)
+                    self.tableArray.append(collection.title)
+                    
+                }
+                /*
+                 let shopifycollection = shopifydata.custom_collections
+                 shopifycollection.forEach { value in
+                 print(self.title!)
+                 
+                 }
+                 */
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            }
+            catch let jsonError {
+                
+            }
+            
+            }.resume()
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+       
+    
+    }
+
+}
+extension ViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = self.tableArray[indexPath.row]
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.tableArray.count
+    }
+    
+}
