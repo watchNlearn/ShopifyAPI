0be826dee947aa656bb9ccedfe1f10b9d21cda2a
Caleb Lee <28caleblee@gmail.com>
2019-01-29 18:01:12 -0800

    Collection VC

---
+++ ShopifyChallenge/CollectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionViewController.swift
+//  ShopifyChallenge
+//
+//  Created by Caleb Lee on 1/29/19.
+//  Copyright © 2019 WithoutAnyLimit. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
