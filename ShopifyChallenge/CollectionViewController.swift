fd70b2943298245b4ca388388963ffdb44508330
Caleb Lee <28caleblee@gmail.com>
2019-02-02 13:49:06 -0800

    Matched Product IDs

--- ShopifyChallenge/CollectionViewController.swift
+++ ShopifyChallenge/CollectionViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
+struct CollectionResponse: Decodable {
+    var collects: [Collects]
+}
+struct Collects: Decodable {
+    var product_id: Int
+}
+//Second Json struct
+struct ProductResponse: Decodable {
+    var products: [Products]
+}
+struct Products: Decodable {
+    var id: Int
+}
+
 class CollectionViewController: UICollectionViewController {
-    
     var cellArrayNames = [String]()
     var collectionTitle: String = ""
     var collectionID: Int!
+    var productIds = [Int]()
+    var actualProductIds = [Int]()
+    var matchedProductIds = [Int]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,16 +39,82 @@ class CollectionViewController: UICollectionViewController {
         // Do JSON Call to get Cell Array List
         //Get prod Ids from https://shopicruit.myshopify.com/admin/collects.json?collection_id=68424466488&page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6
         //then search the ids in https://shopicruit.myshopify.com/admin/products.json?ids=2759137027,2759143811&page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6
-        cellArrayNames = ["Hammer", "Brick", "Shovel"]
+        
+        let jsonUrlString = "https://shopicruit.myshopify.com/admin/collects.json?collection_id=68424466488&page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6"
+        guard let url = URL(string: jsonUrlString) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else {
+                return
+            }
+            do {
+                let shopifydata = try JSONDecoder().decode(CollectionResponse.self, from: data)
+                for collect in shopifydata.collects {
+                    //print(collect.product_id)
+                    self.productIds.append(collect.product_id)
+                }
+            }
+            catch let jsonError {
+            print("JSON Error")
+            }
+        }.resume()
+        
+        
+       
+        //Second JSON Call
+        let jsonUrlString2 = "https://shopicruit.myshopify.com/admin/products.json?ids=2759137027,2759143811&page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6"
+        guard let url2 = URL(string: jsonUrlString2) else {
+            return
+        }
+        URLSession.shared.dataTask(with: url2) {(data, response, error) in
+            guard let data2 = data else {
+                return
+            }
+            do {
+                let shopifydata2 = try JSONDecoder().decode(ProductResponse.self, from: data2)
+                for product in shopifydata2.products {
+                    //print(product.id)
+                    self.actualProductIds.append(product.id)
+                }
+            }
+            catch let jsonError {
+                print("JSON Error")
+            }
+            }.resume()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            print(self.productIds)
+            print("~~~~~~~~~~~")
+            print(self.actualProductIds)
+            for i in self.actualProductIds {
+                if self.productIds.contains(i){
+                print(i)
+                    self.matchedProductIds.append(i)
+                }
+                self.collectionView.reloadData()
+                
+            }
+        }
+        
+        
+        
+        cellArrayNames = ["Hammer", "Brick"]
         // Do any additional setup after loading the view.
     }
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        //return actualProductIds.count
+        return self.actualProductIds.count
+        
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         var cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
         var cellProdductNames = cell.viewWithTag(1) as! UILabel
         cellProdductNames.text = cellArrayNames[indexPath.row]
+        var cellCollectionName = cell.viewWithTag(2) as! UILabel
+        cellCollectionName.text = collectionTitle
         
         
         return cell
