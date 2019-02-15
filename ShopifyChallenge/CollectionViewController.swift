6c03df8dba3abdb3eaa0bf79ea736c19f0549f0c
Caleb Lee <28caleblee@gmail.com>
2019-02-14 20:05:23 -0800

    Edits

--- ShopifyChallenge/CollectionViewController.swift
+++ ShopifyChallenge/CollectionViewController.swift
@@ -47,7 +47,7 @@ class CollectionViewController: UICollectionViewController {
         print("~~~~ Collection View ~~~~")
         print(collectionTitle)
         print(collectionID)
-        
+        //First JSON Call
         let jsonUrlString = "https://shopicruit.myshopify.com/admin/collects.json?collection_id=68424466488&page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6"
         guard let url = URL(string: jsonUrlString) else {
             return
